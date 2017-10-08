f00146f63c5fed662d777ac0a4dec526de068bbe
Robin Sikkens <Robinsikkens@live.nl>
2017-10-08 21:27:20 +0200

    xkcd support!

---
+++ stickord/commands/xkcd.py
@@ -0,0 +1,22 @@
+'''
+Provides commands to the xkcd system
+'''
+from stickord.helpers.xkcd_api import get_random, get_by_id, print_comic, get_recent
+from stickord.registry import Command
+
+@Command('xkcd', category='xkcd')
+async def get_comic(cont, _mesg):
+    ''' Search for a comic by id, if no id is provided it will post a random comic. '''
+    if len(cont) != 0:
+        id = int(cont[0])
+        print(id)
+        comic = await get_by_id(id)
+        return await print_comic(comic)
+
+    comic = await get_random()
+    return await print_comic(comic)
+
+@Command('newxkcd', category='xkcd')
+async def get_latest_comic(_cont, _mesg):
+    ''' Posts the latest xkcd comic. '''
+    return await get_recent()
---
+++ stickord/helpers/xkcd_api.py
@@ -0,0 +1,37 @@
+'''
+Provides some help talking to the xkcds API
+'''
+
+import json
+import requests
+import random
+import discord
+
+async def get_recent():
+    ''' Get and parse the most recent xkcd comic. '''
+    response = requests.get('https://xkcd.com/info.0.json')
+    return json.loads(response.text)
+
+async def get_by_id(id):
+    ''' Get and parse an xkcd comic based on the comic id. '''
+    response = requests.get(f'https://xkcd.com/{id}/info.0.json')
+    return json.loads(response.text)
+
+async def get_random():
+    ''' Get and parse a random xkcd comic. '''
+    latest = get_recent()
+    max_id = latest['num']
+    rnd = random.randint(1, max_id)
+    if rnd == 404
+        return get_random()
+    return get_by_id(rnd)
+
+
+async def print_comic(comic):
+    ''' Format an xkcd comic to be displayed in an Embed. '''
+    embed = discord.Embed(color=discord.Colour(0xffffff))
+    embed.title = comic['safe_title']
+
+    embed.set_image(url=comic['img'])
+
+    return embed
