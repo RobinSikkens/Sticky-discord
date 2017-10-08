606b7f17e0aa2b3d4978b9f73dc80c7e83a31977
Maarten van den Berg <maartenberg1@gmail.com>
2017-10-08 23:23:07 +0200

    Shutdown command

---
+++ stickord/commands/shutdown.py
@@ -0,0 +1,17 @@
+'''
+Provides command to shut down bot in case of trouble.
+'''
+import os
+import signal
+
+from stickord.registry import get_easy_logger, Command, whitelist_only
+
+
+LOGGER = get_easy_logger('commands.shutdown')
+
+@Command('botshutdown', hidden=True)
+@whitelist_only(['Admin', 'Moderator'])
+async def shutdown(_cont, mesg, *args, **kwargs):
+    ''' Shutdown bot by sending SIGINT. '''
+    LOGGER.critical('Shutdown initiated by {mesg.author.mention}')
+    os.kill(os.getpid(), signal.SIGQUIT)
