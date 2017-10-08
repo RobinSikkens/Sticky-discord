6e75cdfb42d7fd7150113e45bc4a24b9e62ce7c4
Robin Sikkens <Robinsikkens@live.nl>
2017-10-08 19:29:32 +0200

    Categorizing

--- stickord/commands/spork.py
+++ stickord/commands/spork.py
@@ -1,12 +1,14 @@
 import random
 from stickord.registry import Command
 
-@Command("8ball")
+@Command("8ball", category="Spork")
 async def eightball(cont, mesg):
+    '''The wise magic 8-ball will know what to do with whatever query you might have'''
     with open("eightball_responses.txt", "r") as f:
         response = random.choice(f.readlines())
     return response
 
-@Command(['flip', 'coinflip', 'muntje'])
+@Command(['flip', 'coinflip', 'muntje'], category="Spork")
 async def flipcoin(cont, mesg):
+    '''Flip a coin: Heads, Tails? Some fine dispute settling here'''
     return random.choice(['Heads', 'Tails'])
